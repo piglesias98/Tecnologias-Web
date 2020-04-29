959746a9163cd912800af84ac514e487ef5fdf4c
Paula <pg.iglesias@bi4group.com>
2020-04-29 18:38:55 +0200

    Parametrizar función nav con query strings

--- practicaPHP/comun.php
+++ practicaPHP/comun.php
@@ -1,19 +1,23 @@
 <?php
 function HTMLnav($activo, $identificado){
   echo <<< HTML
-  <nav> <h1>Índice</h1> <ul>
+  <nav>
+    <h1>Índice</h1>
+    <ul>
     HTML;
     if $identificado{
       $items = ['Inicio', 'Listado de recetas', 'Añadir receta nueva','Página de contacto'];
-      $links = ['inicio.php', 'listado.php', 'receta_nueva.php', 'contacto.php'];
+      $links = ['inicio', 'listado', 'receta_nueva', 'contacto'];
     }else{
       $items = ['Inicio', 'Listado de recetas', 'Página de contacto'];
-      $links = ['inicio.php', 'listado.php', 'contacto.php'];
+      $links = ['inicio', 'listado', 'contacto.'];
     }
     foreach ($items as $key => $value)
-      echo "<li".($k==$activo?" class='activo'":"")."<a href=".$links[$k].">".$v."</a></li>";
+      echo "<li".($key==$activo?" class='activo'":"").">".
+      "<a href=index.php?p=".$links[$k].">".$v."</a></li>";
     echo <<< HTML
-  </ul></nav>
-  HTML;
+  </ul>
+</nav>
+HTML;
 }
 ?>
--- practicaPHP/index.php
+++ practicaPHP/index.php
@@ -8,11 +8,12 @@ if (isset($_GET["p"]) && ($_GET["p"]>=0 || $_GET["p"]<=3)){
 
 HTMLinicio("Mi sitio web");
 
-HTMLnav_alternativo($opc);
+HTMLnav($opc);
 switch($opc) {
   case 0: HTMLpag_inicio(); break;
   case 1: HTMLpag_listado(); break;
   case 2: HTMLpag_contacto(); break;
+  case 3: HTMLpag_nueva(); break;
 }
 
  ?>
