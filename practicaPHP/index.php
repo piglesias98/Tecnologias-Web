47cbc6e8b0ad52f3ba20e0a64789eee2073e0d96
Paula <pg.iglesias@bi4group.com>
2020-05-01 15:17:21 +0200

    Funciona la navegación pero no pinta el link

--- practicaPHP/index.php
+++ practicaPHP/index.php
@@ -1,19 +1,24 @@
 <?php
 require "pag_comun.php";
+include "header.html";
 
-$opc = 0;
-if (isset($_GET["p"]) && ($_GET["p"]>=0 || $_GET["p"]<=3)){
-  &opc = $_GET['p'];
+// Obtener el query string
+
+if ((!isset($_GET["p"])) || ($_GET["p"]<0 || $_GET["p"]>3)){
+  $_GET['p'] = 0;
 }
 
-HTMLinicio("Mi sitio web");
+$opc = $_GET['p'];
 
+$identificado = False;
+//Según el query string resaltamos el menú
 HTMLnav($opc);
+//Según el query string insertamos el código HTML correspondiente
 switch($opc) {
-  case 0: HTMLpag_inicio(); break;
-  case 1: HTMLpag_listado(); break;
-  case 2: HTMLpag_contacto(); break;
-  case 3: HTMLpag_nueva(); break;
+  case 0: include "inicio.html"; break;
+  case 1: include "listado.html"; break;
+  case 2: include "contacto.html"; break;
+  case 3: include "pag_nueva.html"; break;
 }
-
+include "footer.html";
  ?>
--- practicaPHP/pag_comun.php
+++ practicaPHP/pag_comun.php
@@ -1,56 +1,14 @@
 <?php
-function HTMLinicio($titulo){
-  echo <<< HTML
-  <!DOCTYPE html>
-  <html lang="en" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <meta name="author" content="Paula Iglesias Ahualli">
-      <meta name="viewport" content="width=device-width">
-      <link rel="stylesheet" href="style.css">
-      <title>$titulo</title>
-    </head>
-    <body>
-  HTML;
-}
-
-function HTMLfin(){
-  echo <<< HTML
-</body>
-</html>
-HTML;
-}
-
-function HTMLfooter(){
-  echo <<< HTML
-  <footer>
-    <p>  © 2020 Tecnologías Web</p>
-    <p class="divisor">|</p>
-    <p>Mapa del sitio</p>
-    <p class="divisor">|</p>
-    <p>Contacto</p>
-  </footer>
-  HTML;
-}
-
-function HTMLnav($activo, $identificado){
-  echo <<< HTML
+function HTMLnav($activo){
+echo <<<HTML
   <nav>
-    <h1>Índice</h1>
-    <ul>
-    HTML;
-    if $identificado{
-      $items = ['Inicio', 'Listado de recetas', 'Añadir receta nueva','Página de contacto'];
-      // $links = ['inicio', 'listado', 'receta_nueva', 'contacto'];
-    }else{
-      $items = ['Inicio', 'Listado de recetas', 'Página de contacto'];
-      // $links = ['inicio', 'listado', 'contacto.'];
-    }
-    foreach ($items as $key => $value)
-      echo "<li".($key==$activo?" class='activo'":"").">".
-      "<a href=index.php?p=".($k).">".$value."</a></li>";
-    echo <<< HTML
-  </ul>
+HTML;
+
+$items = ['Inicio', 'Listado de recetas', 'Página de contacto'];
+foreach ($items as $key => $value)
+  echo "<a".($key==$activo?" class='activo'":"").
+  " href=index.php?p=".($key).">".$value."</a>";
+echo <<< HTML
 </nav>
 HTML;
 }
