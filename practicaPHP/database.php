f2724b90c7031521b01d0622431fa6d643b6b1c9
Paula <pg.iglesias@bi4group.com>
2020-05-01 20:58:00 +0200

    casi consigo conectarme pero no

--- practicaPHP/database.php
+++ practicaPHP/database.php
@@ -1,11 +1,25 @@
+<?php
 $host="localhost";
 $port=3306;
 $socket="";
-$user="root";
-$password="";
+$user="paula";
+$password="paula";
 $dbname="receta";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
 	or die ('Could not connect to the database server' . mysqli_connect_error());
 
+  $query = "SELECT titulo, autor, categoria, descripcion, ingredientes, preparacion, fotografia FROM receta";
+
+
+  if ($stmt = $con->prepare($query)) {
+      $stmt->execute();
+      $stmt->bind_result($titulo, $autor, $categoria, $descripcion, $ingredientes, $preparacion, $fotografia);
+      while ($stmt->fetch()) {
+          printf("%s, %s, %s, %s, %s, %s, %s\n", $titulo, $autor, $categoria, $descripcion, $ingredientes, $preparacion, $fotografia);
+      }
+      $stmt->close();
+  }
+
 //$con->close();
+?>
