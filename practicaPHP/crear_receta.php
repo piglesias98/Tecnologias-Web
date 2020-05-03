7c0e15b1f248d4d3221bec51c3df6df60a55011d
Paula <pg.iglesias@bi4group.com>
2020-05-03 18:01:00 +0200

    Puedo mostrar la imagen preliminar!!!!!!!!!!!

--- practicaPHP/crear_receta.php
+++ practicaPHP/crear_receta.php
@@ -2,19 +2,24 @@
 <h3>Crear una receta</h3>
 <?php
 //Obtener y validar parámetros
-$params = getParams($_POST, $FILES);
+$params = getParams($_POST, $_FILES);
 //Si se han recibido los datos y son correctos
 if ($params['enviado']==true && $params['err_titulo']=='' && $params['err_autor']==''
     && $params['err_categoria']=='' && $params['err_descripcion'] ==''
-    && $params['err_ingredientes']=='' && $params['err_preparacion']==''){
+    && $params['err_ingredientes']=='' && $params['err_preparacion']==''
+    && $params['err_preparacion']=='' && $params['err_fotografia'] == ''){
   solicitarConfirmacion($params);
+}else if (isset($params['confirmacion'])){
+  enviarFormulario($params);
 }else{
   //Si no se han recibido parámetros o son incorrectos
   showForm($params);
 }
 
 function getParams($p, $f){
-  if (isset($p['titulo']) or isset($p['autor']) or isset($p['categoria']) or isset($p['descripcion'])
+  if (isset($p['confirmar'])){
+    $result['confirmar'] = true;
+  }else if (isset($p['titulo']) or isset($p['autor']) or isset($p['categoria']) or isset($p['descripcion'])
       or isset($p['ingredientes']) or isset($p['preparacion']) or isset($f['fotografia'])){
     $result['enviado'] = true;
     // Validación de resultados
@@ -23,7 +28,7 @@ function getParams($p, $f){
     if (empty($p['titulo'])){
       $result['err_titulo'] = 'El título no puede estar vacío';
     }else{
-      $result['nombre'] = $p['nombre'];
+      $result['titulo'] = $p['titulo'];
     }
     // -> autor
     $result['err_autor'] = '';
@@ -61,20 +66,26 @@ function getParams($p, $f){
       $result['preparacion'] = $p['preparacion'];
     }
     // -> fotografía
-    $target_dir = "uploads/";
-    $target_file = $target_dir . basename($f["fotografia"]["nombre_foto"]);
-    $uploadOk = 1;
-    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     $result['err_fotografia']='';
-    // Comprobar que se trata de verdad de una imagen
-    if (!getimagesize($f["fotografia"]["nombre_foto"])){
-      $result['err_fotografia'] = "El archivo no es una fotografía";
-    // Comprobar el tamaño del archivo
-    }else if ($f["fileToUpload"]["size"] > 500000){
-      $result['err_fotografia'] = 'Lo siento, el archivo es demasiado grande';
-    // Sólo permitimos JPG, JPEG y PNG
-    }else if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"){
-      $result['err_fotografia'] = 'Lo siento, solo se permiten archivos PNG, JPG o JPEG';
+    if(empty($f['fotografia'])){
+      $result['err_fotografia'] = 'Debe incluir una fotografía';
+    }else{
+      $target_dir = "uploads/";
+      $target_file = $target_dir . basename($f['fotografia']['name']);
+      $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+      // Comprobar que se trata de verdad de una imagen
+      if (!getimagesize($f['fotografia']['tmp_name'])){
+        $result['err_fotografia'] = "El archivo no es una fotografía";
+      // Comprobar el tamaño del archivo
+      }else if ($f['fotografia']["size"] > 500000){
+        $result['err_fotografia'] = 'Lo siento, el archivo es demasiado grande';
+      // Sólo permitimos JPG, JPEG y PNG
+      }else if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"){
+        $result['err_fotografia'] = 'Lo siento, solo se permiten archivos PNG, JPG o JPEG';
+      }else{
+        $result['fotografia'] = $f['fotografia'];
+        $result['fotografia_temp'] = file_get_contents($f['fotografia']['tmp_name']);
+      }
     }
   } else {
     //El formulario aún no ha sido enviado
@@ -87,26 +98,41 @@ function getParams($p, $f){
 
 function showForm($params){
   ?>
-  <form class="login_form" action="index.php?p=2" method="post">
-    <label for="nombre">Nombre:
-      <input type="text" name="nombre"
-      <?php if (isset($params['nombre'])) echo " value='".$params['nombre']."'";?>/>
-      <?php if (isset($params['err_nombre'])) echo "<p class = 'error'>".$params['err_nombre'];?></p>
+  <form class="login_form" action="index.php?p=3" enctype="multipart/form-data" method="post">
+    <label for="nombre">Título de la receta:
+      <input type="text" name="titulo"
+      <?php if (isset($params['titulo'])) echo " value='".$params['titulo']."'";?>/>
+      <?php if (isset($params['err_titulo'])) echo "<p class = 'error'>".$params['err_titulo'];?></p>
     </label>
-    <label for="email">Correo Electrónico:
-      <input type="text" name="email"
-      <?php if (isset($params['email'])) echo " value='".$params['email']."'";?>/>
-      <?php if (isset($params['err_email'])) echo "<p class = 'error'>".$params['err_email'];?></p>
+    <label for="autor">Autor:
+      <input type="text" name="autor"
+      <?php if (isset($params['autor'])) echo " value='".$params['autor']."'";?>/>
+      <?php if (isset($params['err_autor'])) echo "<p class = 'error'>".$params['err_autor'];?></p>
     </label>
-    <label for="email">Teléfono:
-      <input type="text" name="telefono"
-      <?php if (isset($params['telefono'])) echo " value='".$params['telefono']."'";?>/>
-      <?php if (isset($params['err_tel'])) echo "<p class = 'error'>".$params['err_tel'];?></p>
+    <label for="categoria">Categoría:
+      <input type="text" name="categoria"
+      <?php if (isset($params['categoria'])) echo " value='".$params['categoria']."'";?>/>
+      <?php if (isset($params['err_categoria'])) echo "<p class = 'error'>".$params['err_categoria'];?></p>
     </label>
-    <label for="comentario">Comentario:
-      <input type="text" name="comentario"
-      <?php if (isset($params['comentario'])) echo " value='".$params['comentario']."'";?>/>
-      <?php if (isset($params['err_com'])) echo "<p class = 'error'>".$params['err_com'];?></p>
+    <label for="descripcion">Descripción:
+      <input type="text" name="descripcion"
+      <?php if (isset($params['descripcion'])) echo " value='".$params['descripcion']."'";?>/>
+      <?php if (isset($params['err_descripcion'])) echo "<p class = 'error'>".$params['err_descripcion'];?></p>
+    </label>
+    <label for="ingredientes">Ingredientes:
+      <input type="text" name="ingredientes"
+      <?php if (isset($params['ingredientes'])) echo " value='".$params['ingredientes']."'";?>/>
+      <?php if (isset($params['err_ingredientes'])) echo "<p class = 'error'>".$params['err_ingredientes'];?></p>
+    </label>
+    <label for="preparacion">Preparación:
+      <input type="text" name="preparacion"
+      <?php if (isset($params['preparacion'])) echo " value='".$params['preparacion']."'";?>/>
+      <?php if (isset($params['err_preparacion'])) echo "<p class = 'error'>".$params['err_preparacion'];?></p>
+    </label>
+    <label for="imagen">Selecciona una imagen:
+      <input type="file" name="fotografia"
+      <?php if (isset($params['fotografia'])) echo "<img src='".$params['fotografia']['tmp_name']."'";?>/>
+      <?php if (isset($params['err_fotografia'])) echo "<p class = 'error'>".$params['err_fotografia'];?></p>
     </label>
     <input type="submit" value="Enviar">
   </form>
@@ -114,17 +140,45 @@ function showForm($params){
 }
 
 
-function showResults($params){
+function enviarFormulario($params){
   ?>
-  <p>Muchas gracias, <?php echo $params['nombre'] ?></p>
-  <p>Contactaremos contigo a la mayor brevedad</p>
-  <p>Estos son los datos que hemos recibido:</p>
-  <ul>
-    <li>Nombre: <?php echo $params['nombre'] ?></li>
-    <li>Correo electrónico: <?php echo $params['email'] ?></li>
-    <li>Teléfono: <?php echo $params['telefono'] ?></li>
-    <li>Comentario: <?php echo $params['comentario'] ?></li>
-  </ul>
+  <p>Muchas gracias, <?php echo $params['autor'] ?></p>
+  <p>Tu receta <?php echo $params['titulo']?> ya está en nuestra base de dato
+      y pronto podrás ver la en la página web :)</p>
+  <?php
+  if (move_uploaded_file($params["fotografia"]["tmp_name"], $target_file)) {
+    $result['err_fotografia'] = 'La foto ha sido subida';
+    $result['fotografia'] = $f['fotografia'];
+  }else{
+    $result['err_fotografia'] = 'Ha habido un problema subiendo la foto';
+  }
+}
+
+function solicitarConfirmacion($params){
+?>
+<form class="login_form" action="index.php?p=3" method="post">
+  <label for="titulo">Título de la receta:
+    <p><?php echo $params['titulo'];?></p>
+  </label>
+  <label for="autor">Autor:
+    <p><?php echo $params['autor'];?></p>
+  </label>
+  <label for="email">Categoría:
+    <p><?php echo $params['categoria'];?></p>
+  <label for="comentario">Descripción:
+    <p><?php echo $params['descripcion'];?></p>
+  </label>
+  <label for="ingredientes">Ingredientes:
+    <p><?php echo $params['ingredientes'];?></p>
+  </label>
+  <label for="preparacion">Preparación:
+    <p><?php echo $params['preparacion'];?></p>
+  </label>
+  <label for="imagen">Selecciona una imagen:
+    <?php echo "<img src='data:image/png;base64,".base64_encode($params['fotografia_temp'])."'";?>/>
+  </label>
+  <input type="submit" value="Confirmar">
+</form>
 <?php
 }
 
